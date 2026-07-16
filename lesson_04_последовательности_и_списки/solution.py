9356f42dc57050b48cbe5f2d1c3ad6c1afc3a364
252116 <252116@k733.edu>
2026-07-16 13:51:07 +0300

    Сделал дз 4

--- lesson_04_последовательности_и_списки/solution.py
+++ lesson_04_последовательности_и_списки/solution.py
@@ -4,17 +4,25 @@
 
 # ---- Задача 4.1: Вставка через срез ----
 def insert_at(lst, i, items):
+    return lst[:i] + items + lst[i:]
     ...
 
 # ---- Задача 4.2: Независимая таблица ----
 def make_grid(rows, cols):
-    ...
+    return [[0] * cols for x in range(rows)]
 
 # ---- Задача 4.3: Через один задом наперёд ----
 def every_other_reversed(lst):
+    lst = lst[::-1]
+    lst = lst[::2]
+    return lst
     ...
 
 # ---- Задача 4.4: Разгладить на один уровень ----
 def flatten_once(nested):
+    lst = []
+    for i in nested:
+        lst += i
+    return lst
     ...
 
