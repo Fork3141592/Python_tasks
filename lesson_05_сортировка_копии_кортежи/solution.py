0ebfcc7f75f2e188fd63b16ba46f3274cd3b2599
252116 <252116@k733.edu>
2026-07-20 00:18:22 +0300

    Сделал дз 5

--- lesson_05_сортировка_копии_кортежи/solution.py
+++ lesson_05_сортировка_копии_кортежи/solution.py
@@ -6,19 +6,25 @@ from typing import Any
 
 
 # ---- Задача 5.1: Сортировка по длине, потом по алфавиту ----
-def sort_words(words: list[str]) -> list[str]: ...
-
+def sort_words(words: list[str]) -> list[str]: 
+    return sorted(words,key=lambda x: (len(x), x))
+    ...
 
 # ---- Задача 5.2: Таблица лидеров (стабильность сортировки) ----
-def leaderboard(rows: list[tuple[str, int]]) -> list[tuple[str, int]]: ...
-
+def leaderboard(rows: list[tuple[str, int]]) -> list[tuple[str, int]]: 
+    return sorted(rows,key = lambda x: (-x[1],x[0]))
+    ...
 
 # ---- Задача 5.3: Настоящая независимая копия ----
 import copy
 
-
-def independent_copy(matrix: list[list[int]]) -> list[list[int]]: ...
+def independent_copy(matrix: list[list[int]]) -> list[list[int]]: 
+    return copy.deepcopy(matrix)
+    ...
 
 
 # ---- Задача 5.4: Неизменяемый кортеж с изменяемым нутром ----
-def append_to_tuple_list(t: tuple[list[Any], Any], x: Any) -> tuple[list[Any], Any]: ...
+def append_to_tuple_list(t: tuple[list[Any], Any], x: Any) -> tuple[list[Any], Any]:
+    t[0].append(x)
+    return t
+    ...
